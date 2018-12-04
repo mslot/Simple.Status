102026d748bc98d8ac9ba6b25db954f7780aacbe
Martin Slot <slot@sigsys.dk>
2018-12-04 20:57:03 +0100

    Refactored a bit

--- Simple.Status.Console/Program.cs
+++ Simple.Status.Console/Program.cs
@@ -27,7 +27,7 @@ namespace Simple.Status.Console
             {
                 string sql = databaseConfig.Sql;
                 string inputString = inputConfig.FormattedInput;
-                List<string> outputStrings = new List<string>();
+                var outputStrings = new List<string>();
                 List<string> columns = GetColumnsFromInputString(inputString);
 
                 using (var command = new System.Data.SqlClient.SqlCommand(sql))
